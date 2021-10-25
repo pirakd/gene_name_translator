fbac6a2b42e0a8f65a022eb8c0cc2e97ae7fdd5d
pirakd <dpirak@gmail.com>
2021-10-25 17:06:39 +0300

    removed custom class to ease pickling, each symbols row is kept(even if appear as alias afterward)

--- gene_translator.py
+++ gene_translator.py
@@ -1,22 +1,19 @@
 import pandas as pd
 import pickle as pl
 import tqdm
+from os import path
+import sys
 
-
-class DictionaryEntry:
-    def __init__(self, entrez_id, uniprot, ensembl_gene_id, symbol, symbol_aliases):
-        self.entrez_id = entrez_id
-        self.uniprot = uniprot
-        self.ensembl_gene_id = ensembl_gene_id
-        self.symbol = symbol
-        self.symbol_aliases = symbol_aliases
 
 class GeneTranslator:
     def __init__(self, verbosity=True):
+        self.root_folder = path.dirname(path.realpath(__file__))
         self.raw_data_folder = 'hgnc_complete_set.txt'
         self.dictionary_file_name = 'gene_dictionary.pl'
+        self.dictionary_file_path = path.join(self.root_folder, self.dictionary_file_name)
         self.dictionary = None
-        self.verbosity = True
+        self.verbosity = verbosity
+
     def translate(self, query, query_type, return_type):
         keys_not_found = list()
         targets_not_found = list()
@@ -24,22 +21,22 @@ class GeneTranslator:
         dictionary = self.dictionary[query_type]
         for q in query:
             if q in dictionary:
-                result = getattr(dictionary[q], return_type)
+                result = dictionary[q][return_type]
                 if result is None:
                     targets_not_found.append(q)
-                result_dict[q] = getattr(dictionary[q], return_type)
+                result_dict[q] = dictionary[q][return_type]
             else:
                 keys_not_found.append(q)
 
         if len(keys_not_found):
-            print('genes {} were not found ({} genes)'.format(keys_not_found, len(keys_not_found)))
+            print('{} genes were not found ({}))'.format(len(keys_not_found), keys_not_found))
         if len(targets_not_found):
-            print('no translations for genes {} ({} genes)'.format(targets_not_found, len(targets_not_found)))
+            print('{} translations are missing ({})'.format(len(targets_not_found), targets_not_found))
 
         return result_dict
 
     def load_dictionary(self):
-        with open(self.dictionary_file_name, 'rb') as f:
+        with open(self.dictionary_file_path, 'rb') as f:
             self.dictionary = pl.load(f)
 
     def generate_dictionaries(self, keys=None):
@@ -49,7 +46,7 @@ class GeneTranslator:
         dictionaries = dict()
         for key in keys:
             dictionaries[key] = self.generate_dictionary(key)
-        with open(self.dictionary_file_name, 'wb') as f:
+        with open(self.dictionary_file_path, 'wb') as f:
             pl.dump(dictionaries, f)
         return dictionaries
 
@@ -67,22 +64,26 @@ class GeneTranslator:
             row_values['ensemble_gene_id'] = row.ensembl_gene_id if not pd.isnull(row.ensembl_gene_id) else None
             if not pd.isnull(row_values[key]):
                 symbol = row.symbol
+                aliases = []
+                prev_symbols = []
                 if not pd.isnull(row.alias_symbol):
                     aliases = row.alias_symbol.split('|')
-                    aliases = [symbol] + aliases
-                else:
-                    aliases = [symbol]
+                if not pd.isnull(row.prev_symbol):
+                    prev_symbols = row.prev_symbol.split('|')
+
+                aliases = list(set([symbol] + aliases + prev_symbols))
                 entrez_id = int(row.entrez_id) if not pd.isnull(row.entrez_id) else None
                 uniprot = row.uniprot_ids if not pd.isnull(row.uniprot_ids) else None
                 ensembl_gene_id = row.ensembl_gene_id if not pd.isnull(row.ensembl_gene_id) else None
 
                 if key == 'symbol':
                     for alias in aliases:
-                        dictionary[alias] = DictionaryEntry(entrez_id=entrez_id, uniprot=uniprot,
-                                                            ensembl_gene_id=ensembl_gene_id, symbol=symbol,
-                                                            symbol_aliases=aliases)
+                        if alias == symbol or alias not in dictionary:
+                            dictionary[alias] = dict(entrez_id=entrez_id, uniprot=uniprot,
+                                                     ensembl_gene_id=ensembl_gene_id, symbol=symbol,
+                                                     symbol_aliases=aliases)
                 else:
-                    dictionary[row_values[key]] = DictionaryEntry(entrez_id=entrez_id, uniprot=uniprot,
-                                                                  ensembl_gene_id=ensembl_gene_id, symbol=symbol,
-                                                                  symbol_aliases=aliases)
+                    dictionary[row_values[key]] = dict(entrez_id=entrez_id, uniprot=uniprot,
+                                             ensembl_gene_id=ensembl_gene_id, symbol=symbol,
+                                             symbol_aliases=aliases)
         return dictionary
