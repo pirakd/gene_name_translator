048f5c85e5804ce864d1f859ca233ac26626a5e2
pirakd <dpirak@gmail.com>
2021-12-07 11:48:22 +0200

    switched dataset, only entrez and symbol are now supported

--- example.py
+++ example.py
@@ -1,12 +1,18 @@
 from gene_translator import GeneTranslator
 
-
 if __name__ == '__main__':
+
     translator = GeneTranslator()
     # this should be done only once after clone
     translator.generate_dictionaries()
     translator.load_dictionary()
-    a = translator.translate([23, 1], 'entrez_id', 'symbol')
-    b = translator.translate([a[23], a[1], 46], 'symbol', 'entrez_id')
-    print(a)
-    print(b)
+
+    entrez_to_symbol = translator.translate([339457, 46], 'entrez_id', 'symbol')
+    symbol_to_entrez = translator.translate([entrez_to_symbol[339457], entrez_to_symbol[46]], 'symbol', 'entrez_id')
+    print(entrez_to_symbol)
+    print(symbol_to_entrez)
+
+    # 339457 is an obsolete entrez_id that was replaced by another id.
+    # translating it would return the up to date symbol.
+    # translating the updated symbol of 339457 will result in an updated entrez_id.
+    # this means that 339457 != symbol_to_entrez[entrez_to_symbol[339457]]
--- gene_translator.py
+++ gene_translator.py
@@ -2,31 +2,43 @@ import pandas as pd
 import pickle as pl
 import tqdm
 from os import path
+from collections import defaultdict
 
 
 class GeneTranslator:
     def __init__(self, verbosity=True):
         self.root_folder = path.dirname(path.realpath(__file__))
-        self.raw_data_file = 'hgnc_complete_set.txt'
+        self.raw_data_file = 'Homo_sapiens.gene_info'
         self.dictionary_file_name = 'gene_dictionary.pl'
+        self.old_names_file = 'gene_history.txt'
         self.raw_data_file_path = path.join(self.root_folder, self.raw_data_file)
         self.dictionary_file_path = path.join(self.root_folder, self.dictionary_file_name)
+        self.old_names_file_path = path.join(self.root_folder, self.old_names_file)
         self.dictionary = None
+        self.old_names_mapping = None
         self.verbosity = verbosity
+        self.use_old_names = True
+        self.new_to_old_keys_mapping = {'symbol': 'obsolete_symbol',
+                               'entrez_id': 'obsolete_entrez_id'}
 
     def translate(self, query, query_type, return_type):
         keys_not_found = list()
         targets_not_found = list()
         result_dict = dict()
         dictionary = self.dictionary[query_type]
+        i = 0
         for q in query:
             if q in dictionary:
                 result = dictionary[q][return_type]
                 if result is None:
                     targets_not_found.append(q)
-                result_dict[q] = dictionary[q][return_type]
             else:
-                keys_not_found.append(q)
+                result = self.query_old_name(q, query_type, return_type)
+                i+=1
+                if result is None:
+                    keys_not_found.append(q)
+
+            result_dict[q] = result
 
         if len(keys_not_found):
             print('{} genes were not found ({}))'.format(len(keys_not_found), keys_not_found))
@@ -40,19 +52,26 @@ class GeneTranslator:
             'Gene dictionary is missing! call GeneTranslator.generate_dictionaries first'
 
         with open(self.dictionary_file_path, 'rb') as f:
-            self.dictionary = pl.load(f)
+            save_dict = pl.load(f)
+        self.dictionary = save_dict['dictionary']
+        self.old_names_mapping = save_dict['old_names_mapping']
         if self.verbosity:
             print('available query types : {}'.format([x for x in self.dictionary.keys()]))
 
     def generate_dictionaries(self, keys=None):
 
-        if keys is None:
-            keys = ['symbol', 'entrez_id', 'uniprot', 'ensemble_gene_id']
+        keys = ['symbol', 'entrez_id']
         dictionaries = dict()
         for key in keys:
             dictionaries[key] = self.generate_dictionary(key)
+
+        old_names_map = self.load_old_names(dictionaries)
+
+        save_dict = {'dictionary': dictionaries,
+                     'old_names_mapping': dict(old_names_map)}
         with open(self.dictionary_file_path, 'wb') as f:
-            pl.dump(dictionaries, f)
+            pl.dump(save_dict, f)
+
         return dictionaries
 
     def generate_dictionary(self, key):
@@ -64,22 +83,16 @@ class GeneTranslator:
         for idx, row in tqdm.tqdm(name_df.iterrows(), desc='Generating dictionary with {} keys'.format(key),
                                   total=len(name_df)):
             row_values['symbol'] = row.symbol
-            row_values['uniprot'] = row.uniprot_ids if not pd.isnull(row.uniprot_ids) else None
             row_values['entrez_id'] = int(row.entrez_id) if not pd.isnull(row.entrez_id) else None
-            row_values['ensemble_gene_id'] = row.ensembl_gene_id if not pd.isnull(row.ensembl_gene_id) else None
             if not pd.isnull(row_values[key]):
                 symbol = row.symbol
                 aliases = []
-                prev_symbols = []
                 if not pd.isnull(row.alias_symbol):
                     aliases = row.alias_symbol.split('|')
-                if not pd.isnull(row.prev_symbol):
-                    prev_symbols = row.prev_symbol.split('|')
 
-                aliases = list(set([symbol] + aliases + prev_symbols))
+                aliases = list(set([symbol] + aliases))
 
-                query_dict = dict(entrez_id=row_values['entrez_id'], uniprot=row_values['uniprot'],
-                                  ensembl_gene_id=row_values['ensemble_gene_id'],
+                query_dict = dict(entrez_id=row_values['entrez_id'],
                                   symbol=row_values['symbol'], symbol_aliases=aliases)
 
                 if key == 'symbol':
@@ -89,3 +102,51 @@ class GeneTranslator:
                 else:
                     dictionary[row_values[key]] = query_dict
         return dictionary
+
+    def load_old_names(self, dictionaries):
+        keys = ['entrez_id', 'obsolete_entrez_id',	'obsolete_symbol']
+
+        old_names_dictionary = {key:dict() for key in keys}
+
+        with open(self.old_names_file_path, 'r') as f:
+            lines = f.readlines()
+            for line in lines[1:]:
+                entrez_id, old_entrez_id , old_symbol = line.strip().split('\t')
+                entrez_id, old_entrez_id = int(entrez_id) if entrez_id != '-' else None, int(old_entrez_id),
+                if entrez_id == '-':
+                    entrez_id = None
+                else:
+                    if entrez_id not in old_names_dictionary[keys[0]]:
+                        old_names_dictionary[keys[0]][entrez_id] = defaultdict(list)
+                    old_names_dictionary[keys[0]][entrez_id][keys[1]].append(old_entrez_id)
+                    old_names_dictionary[keys[0]][entrez_id][keys[2]].append(old_symbol)
+
+                if old_entrez_id not in old_names_dictionary[keys[1]]:
+                    old_names_dictionary[keys[1]][old_entrez_id] = defaultdict(list)
+                if old_symbol not in old_names_dictionary[keys[2]]:
+                    old_names_dictionary[keys[2]][old_symbol] = defaultdict(list)
+
+                # if there is a mapping to an official gene
+                if entrez_id:
+                    old_names_dictionary[keys[1]][old_entrez_id][keys[0]].append(entrez_id)
+                    old_names_dictionary[keys[2]][old_symbol][keys[0]].append(entrez_id)
+                else:
+                    old_names_dictionary[keys[1]][old_entrez_id][keys[2]].append(old_symbol)
+                    old_names_dictionary[keys[2]][old_symbol][keys[1]].append(old_entrez_id)
+
+        return old_names_dictionary
+
+    def query_old_name(self, q, query_type, return_type):
+        query = self.old_names_mapping[self.new_to_old_keys_mapping[query_type]].get(q, None)
+        if query is not None:
+            if 'entrez_id' in query:
+                result = self.dictionary['entrez_id'].get(query['entrez_id'][0], None)
+                if result:
+                    return result[return_type]
+            return query[self.new_to_old_keys_mapping[return_type]][0]
+
+        if query_type == 'entrez_id':
+            query = self.old_names_mapping['entrez_id'].get(q, None)
+            if query:
+                return query[self.new_to_old_keys_mapping[return_type]][0]
+        return None
